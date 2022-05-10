6e00774b0aa16d24f5fa2abc09f3759ebc6b25ff
Burak Alkış <69517719+chamberr3kt@users.noreply.github.com>
2022-05-10 23:04:30 +0300

    Hesap Makinası

---
+++ HesapMakinesi/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		int n1,n2,select;
+		
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Birinci Sayýyý Giriniz");
+		n1= input.nextInt();
+		System.out.println("Ýkinci Sayýyý Giriniz");
+		n2 = input.nextInt();
+		
+		System.out.println("Yapmak Ýstediðiniz Ýþlemi Giriniz 1-Toplama\n 2-Çýkarma\n 3-Çarpma\n 4-Bölme ");
+        select = input.nextInt();		
+		int sonuc = 0;
+		
+		switch (select) {
+		case 1:  sonuc =n1+n2; break;
+		case 2:  sonuc =n1-n2; break;	
+		case 3:  sonuc =n1*n2; break;	
+		case 4:  sonuc =n1/n2; break;
+		default:
+			break;
+		}
+		
+		System.out.println(sonuc);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
